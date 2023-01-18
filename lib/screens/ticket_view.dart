458e616374ee7a8f865bd9af261901960b84de95
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-01-18 12:53:28 +0100

    [Card build Layout 40%]

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:tickets/utils/app_layout.dart';
 import 'package:tickets/widgets/thick_container.dart';
 import '../utils/app_styles.dart';
@@ -32,30 +33,55 @@ class TicketView extends StatelessWidget{
                       Expanded(child: Container()),
                       const ThickContainer(),
 
-                      Expanded(child: SizedBox(
-                        height: 24,
-                        child: LayoutBuilder(
-                          builder: (BuildContext context, BoxConstraints constraints) {
-                            print("The Width is ${constraints.constrainWidth()}");
-                           return Flex(
-                              direction: Axis.horizontal,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: List.generate((constraints.constrainWidth()/6).floor(), (index) =>
-                              const SizedBox(
-                                width: 3,
-                                height: 1,
-                                child: DecoratedBox(decoration: BoxDecoration(
-                                  color: Colors.white
-                                )),
-                              )),
-                            );
-                          },
-                        )
+                      Expanded(child: Stack(
+                        children: [
+                          SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (BuildContext context, BoxConstraints constraints) {
+                                  print("The Width is ${constraints.constrainWidth()}");
+                                  return Flex(
+                                    direction: Axis.horizontal,
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: List.generate((constraints.constrainWidth()/6).floor(), (index) =>
+                                    const SizedBox(
+                                      width: 3,
+                                      height: 1,
+                                      child: DecoratedBox(decoration: BoxDecoration(
+                                          color: Colors.white
+                                      )),
+                                    )),
+                                  );
+                                },
+                              )
+                          ),
+                          Center(child: Transform.rotate(angle: 1.5,
+                              child: const Icon(Icons.local_airport_rounded, color: Colors.white)
+                          )
+                          )
+                        ],
                       )),
-
+                      
                       const ThickContainer(),
                       Expanded(child: Container()),
-                      Text("London", style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                      Text("LDN", style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                    ],
+                  ),
+                  const Gap(16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text("New York", style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                      ),
+                      Text("8H 30M", style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                      SizedBox(
+                        width: 100,
+                        child: Text("London", textAlign: TextAlign.end , style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                      ),
+
                     ],
                   )
                 ],
