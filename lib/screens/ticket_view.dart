a3762be1d0e68c6ae1839f385dccd1a9a31f85c2
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-01-19 11:45:55 +0100

    [Ticket first card 100% completed]

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -98,7 +98,7 @@ class TicketView extends StatelessWidget{
                     width: 10,
                     child: DecoratedBox(
                       decoration: BoxDecoration(
-                        color: Colors.white,
+                        color: Color(0xFFeeedf2),
                         borderRadius: BorderRadius.only(
                           topRight: Radius.circular(10),
                           bottomRight: Radius.circular(10)
@@ -106,36 +106,95 @@ class TicketView extends StatelessWidget{
                       ),
                     ),
                   ),
-                  Expanded(child: LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-                  return  Flex(
-                      direction: Axis.horizontal,
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: List.generate((constraints.constrainWidth()/15).floor(), (index) => const SizedBox(
-                        width: 5,
-                        height: 1,
-                        child: DecoratedBox(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                            )
-                        ),
-                      )),
-                    );
-                  },
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
+                    return  Flex(
+                        direction: Axis.horizontal,
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: List.generate((constraints.constrainWidth()/15).floor(), (index) => const SizedBox(
+                          width: 5,
+                          height: 1,
+                          child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              )
+                          ),
+                        )),
+                      );
+                    },
 
+                    ),
                   )),
                   const SizedBox(
                     height: 20,
                     width: 10,
                     child: DecoratedBox(
                       decoration: BoxDecoration(
-                        color: Colors.white,
+                          color: Color(0xFFeeedf2),
                         borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(10),
                           bottomLeft: Radius.circular(10)
                         )
                       ),
                     ),
+                  )
+                ],
+              ),
+            ),
+          //  bottom part of the orange container
+            Container(
+              padding: const EdgeInsets.only(top: 10, left: 16, bottom: 16, right: 16 ),
+             decoration: BoxDecoration(
+               color: Styles.orangeColor,
+               borderRadius: const BorderRadius.only(
+                 bottomRight: Radius.circular(21),
+                 bottomLeft: Radius.circular(21)
+               )
+             ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                           Text("1 May",
+                            style: Styles.headLineStyle3.copyWith(color: Colors.white
+                            )),
+                          const Gap(5),
+                          Text("Date",
+                          style: Styles.headLineStyle4.copyWith(color: Colors.white
+                          ))
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text("08:00 AM",
+                              style: Styles.headLineStyle3.copyWith(color: Colors.white
+                              )),
+                          const Gap(5),
+                          Text("Departure time",
+                              style: Styles.headLineStyle4.copyWith(color: Colors.white
+                              ))
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                           Text("23",
+                            style: Styles.headLineStyle3.copyWith(color: Colors.white
+                            )),
+                          const Gap(5),
+                          Text("Number",
+                          style: Styles.headLineStyle4.copyWith(color: Colors.white
+                          ))
+                        ],
+                      )
+                    ],
                   )
                 ],
               ),
