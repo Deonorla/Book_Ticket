45fa5f002843452779861db15a6959e97c1f4ec2
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-01-23 10:40:36 +0100

    [Hotel card layout done]

--- lib/screens/hotel_screen.dart
+++ lib/screens/hotel_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:tickets/utils/app_layout.dart';
 import 'package:tickets/utils/app_styles.dart';
 
@@ -16,7 +17,14 @@ class HotelScreen extends StatelessWidget {
       margin: const EdgeInsets.only(top: 5, right: 17),
       decoration: BoxDecoration(
         color: Styles.primaryColor,
-        borderRadius: BorderRadius.circular(12)
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade200,
+            spreadRadius: 5,
+            blurRadius: 20
+          )
+        ]
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -33,7 +41,22 @@ class HotelScreen extends StatelessWidget {
                 )
               ),
             ),
-          )
+          ),
+          const Gap(10),
+          Text(
+              "Open Space",
+            style: Styles.headLineStyle2.copyWith(color: Styles.kakiColor),
+          ),
+          const Gap(5),
+          Text(
+            "London",
+            style: Styles.headLineStyle3.copyWith(color: Colors.white),
+          ),
+          const Gap(8),
+          Text(
+            "\$40/Night",
+            style: Styles.headLineStyle1.copyWith(color: Styles.kakiColor)
+          ),
         ],
       ),
 
